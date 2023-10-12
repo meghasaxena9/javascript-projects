1b1f9f8d2c47832d7eb1916a4f6729eb911b3498
meghasaxena9 <142468846+meghasaxena9@users.noreply.github.com>
2023-10-12 13:38:16 -0500

    Update test-student-labs.js

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,4 +1,5 @@
-function gradeLabs(labs) {
+
+/*function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
@@ -22,3 +23,41 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+*/
+function gradeLabs(labs) {
+  for (let i=0; i < labs.length; i++) {
+      let lab = labs[i];
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+  }
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+try {
+  console.log('Property is not defined');
+}
+catch(err){
+  console.log("ERROR THROWN");
+}
+  
+gradeLabs(studentLabs2);
+
+  
+}
