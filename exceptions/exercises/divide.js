ccaa6071f8e7f4b98cafa66cc3e2ce87caf1388c
megha saxena <meghasaxena9@gmail.com>
2023-10-12 13:35:03 -0500

    commit

---
+++ exceptions/exercises/divide.js
@@ -0,0 +1,12 @@
+// Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+function division(numerator, denominator) {
+    if (denominator === 0) {
+       throw Error('You cannot divide numerator by zero!');
+    }
+    return numerator/denominator;
+ }
+// Your function should return the result of numerator / denominator.
+
+// However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
+
+// Code your divide function here:
---
+++ exceptions/exercises/test-student-labs.js
@@ -0,0 +1,62 @@
+/*function gradeLabs(labs) {
+  for (let i=0; i < labs.length; i++) {
+    let lab = labs[i];
+    let result = lab.runLab(3);
+    console.log(`${lab.student} code worked: ${result === 27}`);
+  }
+}
+
+let studentLabs = [
+  {
+    student: 'Carly',
+    runLab: function (num) {
+        return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Erica',
+    runLab: function (num) {
+        return num * num;
+    }
+  }
+];
+
+gradeLabs(studentLabs);
+*/
+function gradeLabs(labs) {
+  for (let i=0; i < labs.length; i++) {
+      let lab = labs[i];
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+  }
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+try {
+  console.log('Property is not defined');
+}
+catch(err){
+  console.log("ERROR THROWN");
+}
+  
+gradeLabs(studentLabs2);
+
+  
+}
