2769ff07d17e5a5d8ff06bec9340fe60cee5b748
megha saxena <meghasaxena9@gmail.com>
2023-10-16 19:25:20 -0500

    12355

--- unit-testing/exercises/tests/checkFive.test.js
+++ unit-testing/exercises/tests/checkFive.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const checkFive = require('../checkFive.js');
 
 //const checkFive = require('../checkFive.js');
@@ -24,30 +23,3 @@ describe("checkFive", function(){
  });
 
 });
-=======
-const checkFive = require('../checkFive.js');
-
-//const checkFive = require('../checkFive.js');
-
-describe("checkFive", function(){
-
-   test("correct output when passed a number less than 5.", function() {
-      //code here...
-      let output = checkFive(2);
-      expect(output).toEqual("2 is less than 5.");
-   });
-
-   test("correct output when passed value of 5.", function() {
-    //code here...
-    let output = checkFive(5);
-    expect(output).toEqual("5 is equal to 5.");
- });
-
- test("correct output when passed a number greater than 5.", function() {
-    //code here...
-    let output = checkFive(7);
-    expect(output).toEqual("7 is greater than 5.");
- });
-
-});
->>>>>>> a67465628fb073e48f10c79d3333f89fff662e32
