42d6979d7048079db7d83eaa4765e449a1c4d532
meghasaxena9 <142468846+meghasaxena9@users.noreply.github.com>
2023-10-30 15:49:34 -0500

    Update script.js

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -5,6 +5,46 @@ function init () {
 
     // Put your code for the exercises here.
     
+}function init () {
+    const missionAbort = document.getElementById("abortMission");
+    const button = document.getElementById("liftoffButton");
+    const paragraph = document.getElementById("statusReport");
+
+    // Put your code for the exercises here.
+    button.addEventListener( 'click' , event => {
+
+        paragraph.innerHTML = 'Houston!  We have liftoff!';
+    });
+
+
+    missionAbort.addEventListener("mouseover" , event =>  {
+        let element = event.target;
+        missionAbort.style.backgroundColor = "red";
+
+        console.log("target" , element);
+    });
+
+    missionAbort.addEventListener ("mouseout" , event => {
+
+        let element = event.target;
+        missionAbort.style.backgroundColor = "";
+
+        console.log("target" , element);
+    });
+
+    missionAbort.addEventListener("click" , event => {
+        let abort = confirm("Are you sure you want to abort the mission?");
+        if(abort == true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+        else {
+            let url = window.location.href;
+            window.open(url)
+        }
+    });
 }
 
 window.addEventListener("load", init);
+
+
+//window.addEventListener("load", init);
